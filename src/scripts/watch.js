7991e0a128d63629f935bcd0cf51aae86663365c
Bancher <55458229+AlexeiZaits@users.noreply.github.com>
2024-12-05 22:24:56 +0500

    upd:  add normaliseRotatation for watch

--- src/scripts/watch.js
+++ src/scripts/watch.js
@@ -2,6 +2,7 @@ class Clock {
     constructor() {
         this.intervalId = null;
         this.clock = {};
+        this.rotation = { hour: 0, minute: 0, second: 0 };
     }
 
     init() {
@@ -10,19 +11,28 @@ class Clock {
         });
     }
 
+    normalizeRotation(currentDeg, newDeg) {
+        const diff = (newDeg - currentDeg + 360) % 360;
+        return diff > 180 ? currentDeg - (360 - diff) : currentDeg + diff;
+    }
+
     updateClock() {
         const now = new Date();
         const hours = now.getHours();
         const minutes = now.getMinutes();
         const seconds = now.getSeconds();
-        
+
         const secondDeg = 6 * seconds;
         const minuteDeg = 6 * minutes + 0.1 * seconds;
         const hourDeg = 30 * (hours % 12) + 0.5 * minutes;
         
-        this.clock[".hour-hand"].style.transform = `rotate(${hourDeg}deg)`;
-        this.clock[".minute-hand"].style.transform = `rotate(${minuteDeg}deg)`;
-        this.clock[".second-hand"].style.transform = `rotate(${secondDeg}deg)`;
+        this.rotation.second = this.normalizeRotation(this.rotation.second, secondDeg);
+        this.rotation.minute = this.normalizeRotation(this.rotation.minute, minuteDeg);
+        this.rotation.hour = this.normalizeRotation(this.rotation.hour, hourDeg);
+
+        this.clock[".hour-hand"].style.transform = `rotate(${this.rotation.hour}deg)`;
+        this.clock[".minute-hand"].style.transform = `rotate(${this.rotation.minute}deg)`;
+        this.clock[".second-hand"].style.transform = `rotate(${this.rotation.second}deg)`;
     }
 
     startClock() {
